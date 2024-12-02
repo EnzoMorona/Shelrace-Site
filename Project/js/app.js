98eacc2ad62707d626e23222147c4e87d6b07ab4
Enzo Coelho Morona <106177078+EnzoMorona@users.noreply.github.com>
2024-12-02 17:26:08 -0300

    Dia 3

---
+++ Project/js/app.js
@@ -0,0 +1,29 @@
+var MenuItens = document.getElementById("MenuItens");
+var produtoImg = document.getElementById("produtoImg");
+var produtoSec = document.getElementById("produtoSec");
+
+MenuItens.style.maxHeight = "0px";
+
+function menucelular(){
+    if (MenuItens.style.maxHeight == "0px"){
+        MenuItens.style.maxHeight = "200px";
+    }else{
+        MenuItens.style.maxHeight = "0px";
+    }
+};
+
+produtoSec[0].onclick = function(){
+    produtoImg.src = produtoSec[0].src;
+}
+
+produtoSec[1].onclick = function(){
+    produtoImg.src = produtoSec[1].src;
+}
+
+produtoSec[2].onclick = function(){
+    produtoImg.src = produtoSec[2].src;
+}
+
+produtoSec[3].onclick = function(){
+    produtoImg.src = produtoSec[3].src;
+}
